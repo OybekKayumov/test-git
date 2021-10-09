8b851897ac60059c6953a25e211d86f396d0282d
Oybek Kayumov <oybek.kayumov@gmail.com>
2021-10-09 14:55:15 +0500

    brnch TEST3 js upd

--- test.js
+++ test.js
@@ -7,15 +7,40 @@
 // })
 // branch TEST3
 
-function factorial(n) {
-    if (n==0) {
-        return -1
+// function factorial(n) {
+//     if (n==0) {
+//         return -1
+//     }
+//     else if (n==1) {
+//         return 1
+//     } else {
+//         return n * factorial(n-1);
+//     }
+// }
+
+// console.log(factorial(5));
+
+function piglatin(str) {
+    let vowels = ["a","e","i","o","u"];
+    //console.log(str[0], str);
+    for (let i=0; i< str.length; i++) {
+        //if (vowels.includes.(str[0]) )
+        if (str[0].match(vowels) ) {
+            
+            return str + "way";
+        }
+        if (vowels.includes(str[i])) {
+            let constClust =str.substr(0,i);
+            let rest = str.substr(i);
+            return rest + constClust + "ay"
+        }
     }
-    else if (n==1) {
-        return 1
-    } else {
-        return n * factorial(n-1);
-    }
+    return str + "ay"
+
 }
 
-console.log(factorial(5));
+
+console.log(piglatin("oybek"));
+console.log(piglatin("kayumov"));
+console.log(piglatin("glove"));
+console.log(piglatin("rythm"));
